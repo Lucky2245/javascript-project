90dd79f89dd9de223b783842506066b0a35fff04
Lucky2245 <95869708+Lucky2245@users.noreply.github.com>
2022-08-13 18:39:07 -0400

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,6 +1,3 @@
 function hideLinks(){
   document.getElementById("Links").style.display = "none";
-  if(document.getElementById("Links").style.display = "none"){
-    document.getElementById("Links").style.display = "block";
-  }
 }
